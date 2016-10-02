68b296c135275031c0baaed628cc558d2852e3a1
nerdinand <nerdinand@nerdinand.com>
2016-10-02 18:12:04 +0200

    Rewrite tests with nice pipe syntax

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -9,14 +9,16 @@ import Encoding
 all : Test
 all =
     describe "elm-qr-code"
-        [ test "Numeric encoding 1" <|
-            \() ->
-                Expect.equal
-                    "00010000001000000000110001010110011000011"
-                    (Encoding.encodeNumeric "01234567")
-        , test "Numeric encoding 2" <|
-            \() ->
-                Expect.equal
-                    "00010000010000000000110001010110011010100110111000010100111010100101"
-                    (Encoding.encodeNumeric "0123456789012345")
+        [ describe "Encoding.encodeNumeric"
+            [ test "Encode 01234567" <|
+                \() ->
+                    "01234567"
+                        |> Encoding.encodeNumeric
+                        |> Expect.equal "00010000001000000000110001010110011000011"
+            , test "Encode 0123456789012345" <|
+                \() ->
+                    "0123456789012345"
+                        |> Encoding.encodeNumeric
+                        |> Expect.equal "00010000010000000000110001010110011010100110111000010100111010100101"
+            ]
         ]
